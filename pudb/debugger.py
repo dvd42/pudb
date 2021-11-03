74ddfeaa6c721d606bd09fe7a2853c0800871b1e
dvd42 <diegovd0296@gmail.com>
2021-11-03 15:48:00 +0100

    arrow keys for shell history

--- pudb/debugger.py
+++ pudb/debugger.py
@@ -1808,6 +1808,8 @@ class DebuggerUI(FrameVarInfoKeeper):
         self.cmdline_edit_sigwrap.listen("enter", cmdline_exec)
         self.cmdline_edit_sigwrap.listen("ctrl n", cmdline_history_next)
         self.cmdline_edit_sigwrap.listen("ctrl p", cmdline_history_prev)
+        self.cmdline_edit_sigwrap.listen("down", cmdline_history_next)
+        self.cmdline_edit_sigwrap.listen("up", cmdline_history_prev)
         self.cmdline_edit_sigwrap.listen("esc", toggle_cmdline_focus)
 
         self.top.listen("ctrl x", toggle_cmdline_focus)
